f606f67e787d07f230513073bdb66da0e32b7d98
GhaadaAlaa <ghadaalaa2299@gmail.com>
2021-12-18 00:24:15 +0200

    getXML tree
    Comments Added

--- getXML.cpp
+++ getXML.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 #include"xml_tree.h"
 using namespace std;
-
+/* used to search for certain character in a given string and return its index*/
 int get_index(string s, char c)
 {
     for (int i = 0; i < s.length(); i++)
@@ -11,18 +11,19 @@ int get_index(string s, char c)
     }
     return -1;
 }
+/* remove spacing at start of string */
 string ltrim(const string &s)
 {
 	int start = s.find_first_not_of(" \n\r\t\f\v");
 	return (start ==string::npos) ? "" : s.substr(start);
 }
-
+/* remove spacing at end of string */
 string rtrim(const string &s)
 {
 	int end = s.find_last_not_of(" \n\r\t\f\v");
 	return (end ==string::npos) ? "" : s.substr(0, end + 1);
 }
-
+/* remove spacing at start and end of string */
 string trim(const string &s) {
 	return rtrim(ltrim(s));
 }
@@ -30,6 +31,7 @@ string trim(const string &s) {
 /* Return root of XML tree */
 tnode* getXMLTree(string *xml)
 {
+    /* Converting the XML string to vector of strings each element represents a line */
     vector<string> x(200);
     int l = 0;
     int prev = -1, i;
@@ -41,32 +43,29 @@ tnode* getXMLTree(string *xml)
             prev = i + 1;
         }
     }
-    x.push_back((*xml).substr(prev, i - prev));
-    string s, s1, s2 = "";
-    int k = 1;
-    int flag = 0;
-    stack < string > tags;
-    stack < string > children;
-    vector<string> tagss;
+    x.push_back((*xml).substr(prev, i - prev)); /* NOW vector is successfully filled by XML lines */
+
+
+    string s = "", s1 = "";            /* s used to copy each vector element in it  */
+                                       /* s1 used hold value of tag name after extractions from <> or </> */
+    stack < string > tags;             /* tags stack used to hold values of openning tags till corresponding closing tag */
+    stack < string > children;         /* children stack used to hold values of tags represent children to a parent tag */
     for (auto i = x.begin(); i != x.end(); i++)
     {
         s = *i;
-        trim(s);
-        int tagname_start = get_index(s, '<') + 1;
-        int tagname_end = get_index(s, '>') - 1;
-        if (s[tagname_start] == '/')
+        trim(s);                                     /* Remove Spacing at start and end of string */
+        int tagname_start = get_index(s, '<') + 1;   /* index of first character in tag name without <> */
+        int tagname_end = get_index(s, '>') - 1;     /* index of last character in tag name without <> */
+        if (s[tagname_start] == '/')                 /*if found closing tag */
         {
-            s1 = s.substr(tagname_start + 1, tagname_end - tagname_start);
-                       cout<<s1<<" ";
-            tags.pop();
+            s1 = s.substr(tagname_start + 1, tagname_end - tagname_start);   /*extact closing tag's name , this step was needed to save value of last poped tag*/
+            tags.pop();                                                      /*pop from stack last tag */
         }
-        else if (tagname_start == 0 && tagname_end == -2){}
-        else
+        else if (tagname_start == 0 && tagname_end == -2){}                  /*neither openning or closing tag , attribute value */
+        else                                                                 /*if found closing tag */
         {
-            s1 = s.substr(tagname_start, tagname_end - tagname_start + 1);
-            cout<<s1<<" ";
+            s1 = s.substr(tagname_start, tagname_end - tagname_start + 1);   /* extract openning tag name , push it at the stack */
             tags.push(s1);
-
         }
     }
 }
