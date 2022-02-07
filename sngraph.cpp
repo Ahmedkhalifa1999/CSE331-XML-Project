e63b279f94c9f5702828930c88b9c550c0bb17c7
Ahmedkhalifa1999 <ahmedkhalifa1999@gmail.com>
2022-02-07 13:07:48 +0200

    Social Network Parameter constructor

--- sngraph.cpp
+++ sngraph.cpp
@@ -2,5 +2,18 @@
 
 SNgraph::SNgraph(tree<std::string> parsed)
 {
-
+    user newUser;
+    std::list<int> followers;
+    for (tree<std::string>* userNode : parsed.children) {
+        newUser.id = stoi(userNode->getChild(0)->data);
+        newUser.name = userNode->getChild(1)->data;
+        for (tree<std::string>* post : userNode->getChild(2)->children) {
+            newUser.posts.push_back(post->data);
+        }
+        for (tree<std::string>* follower : userNode->getChild(3)->children) {
+            followers.push_back(stoi(follower->data));
+        }
+        addNode(newUser, followers);
+        followers.clear();
+    }
 }
